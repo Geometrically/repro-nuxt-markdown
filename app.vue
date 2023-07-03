beb21f91dbbf2f4098b5074198edd199daafa018
Jai A <jaiagr+gpg@pm.me>
2023-07-03 15:47:31 -0700

    make repro work with markdown

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div v-html="parsed">
   </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+import MarkdownIt from 'markdown-it'
+
+const md = new MarkdownIt('default', {
+    html: true,
+    linkify: true,
+    breaks: false
+  })
+
+const markdown = 'https://modrinth.com'
+
+const parsed = ref(md.render(markdown))
+</script>
